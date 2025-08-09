67b363b6da9141a1af1818eefa6fdf116700cf08
Miloszel <m.branewicz@wp.pl>
2025-08-09 10:33:23 +0200

    add TaskSubmission model

---
+++ app/Models/TaskSubmission.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TaskSubmission extends Model
+{
+    protected $fillable = [
+        'task_id',
+        'user_id',
+        'description',
+        'media',
+        'is_winner'
+    ];
+}
